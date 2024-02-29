5101190223752139e3aa3c421c663dc8ce197727
srb <saurabh.borse@alumni.fh-aachen.de>
2024-02-29 10:44:51 +0100

    commented odom tf broadcast

--- rto_node/src/OdometryROS.cpp
+++ rto_node/src/OdometryROS.cpp
@@ -45,16 +45,17 @@ void OdometryROS::readingsEvent(double x, double y, double phi,
 	odometry_msg_.twist.twist.angular.y = 0.0;
 	odometry_msg_.twist.twist.angular.z = omega;
 
-	odometry_transform_.header.stamp = odometry_msg_.header.stamp;
-	odometry_transform_.transform.translation.x = x;
-	odometry_transform_.transform.translation.y = y;
-	odometry_transform_.transform.translation.z = 0.0;
-	odometry_transform_.transform.rotation.x = phi_quat.getX();
-	odometry_transform_.transform.rotation.y = phi_quat.getY();
-	odometry_transform_.transform.rotation.z = phi_quat.getZ();
-	odometry_transform_.transform.rotation.w = phi_quat.getW();
+	// Commentd: Transform is not needed, being published by the ekf fusion node
+	// odometry_transform_.header.stamp = odometry_msg_.header.stamp;
+	// odometry_transform_.transform.translation.x = x;
+	// odometry_transform_.transform.translation.y = y;
+	// odometry_transform_.transform.translation.z = 0.0;
+	// odometry_transform_.transform.rotation.x = phi_quat.getX();
+	// odometry_transform_.transform.rotation.y = phi_quat.getY();
+	// odometry_transform_.transform.rotation.z = phi_quat.getZ();
+	// odometry_transform_.transform.rotation.w = phi_quat.getW();
 
-	odometry_transform_broadcaster_->sendTransform(odometry_transform_);
+	// odometry_transform_broadcaster_->sendTransform(odometry_transform_);
 
 	// Publish the msg
 	odometry_pub_->publish(odometry_msg_);
