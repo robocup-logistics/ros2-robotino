54ff65d854e0b6da529d8f88a48c496f29c34d09
Tarik Viehmann <viehmann@kbsg.rwth-aachen.de>
2026-03-11 12:57:04 +0100

    rto_node: add absolute axis control instead of action server commands

--- rto_node/scripts/robotino_teleop.py
+++ rto_node/scripts/robotino_teleop.py
@@ -9,6 +9,8 @@ import struct
 import sys
 from rclpy.action import ActionClient
 from gigatino_msgs.action import Move
+from geometry_msgs.msg import Vector3
+from gigatino_msgs.msg import Feedback
 
 class Robotino3Teleop(Node):
 
@@ -27,8 +29,23 @@ class Robotino3Teleop(Node):
         self.client_socket.connect(("192.168.0.100", 6465))
 
         self.move_client = ActionClient(self, Move,'gigatino/move')
+        self.move_pub = self.create_publisher(Vector3, 'gigatino/move_absolute', 10)
 
+        self.feedback_sub = self.create_subscription(
+            Feedback,
+            'gigatino/feedback',
+            self.feedback_callback,
+            10
+        )
+
+        self.current_x = 0.0
+        self.current_yaw = 0.0
+        self.current_z = 0.0
         self.prev_buttons = [0]*12
+
+        self.step_x = 5.0     # mm
+        self.step_z = 5.0     # mm
+        self.step_yaw = 2.0   # deg
 
         # Initialize parameters
         self.declare_parameter('forward_axis_scalling', 1.0)
@@ -36,6 +53,24 @@ class Robotino3Teleop(Node):
 
     def __del__(self):
         self.client_socket.close()
+
+    def feedback_callback(self, msg):
+        self.current_x = msg.stepper_positions[0]
+        self.current_yaw = msg.stepper_positions[1]
+        self.current_z = msg.stepper_positions[2]
+
+    def send_absolute_move(self, x, yaw, z):
+
+        msg = Vector3()
+        msg.x = x
+        msg.y = yaw
+        msg.z = z
+
+        self.move_pub.publish(msg)
+
+        self.get_logger().info(
+            f"Move absolute: x={x:.1f} yaw={yaw:.1f} z={z:.1f}"
+        )
 
     # callback function to publish data over cmd_vel topic based on joy_pad inputs
     def TeleopCallback(self, data):
@@ -59,15 +94,45 @@ class Robotino3Teleop(Node):
         else:
           self.saved_img = False
         # Button press detection
-        if data.buttons[1] == 1 and self.prev_buttons[1] == 0:
-            self.send_move_goal(0.05, 0.05, 0.1)
-            self.prev_buttons = data.buttons
-        if data.buttons[2] == 1 and self.prev_buttons[2] == 0:
-            self.send_move_goal(0.05, 0.05, 0.05)
-            self.prev_buttons = data.buttons
-        if data.buttons[3] == 1 and self.prev_buttons[3] == 0:
-            self.send_move_goal(0.05, 0.05, 0.01)
-            self.prev_buttons = data.buttons
+        if data.buttons[2] == 1:   # X button
+            if data.button[13] == 1:     # D-pad up
+                self.send_absolute_move(self.current_x + self.step_x,
+                                        self.current_yaw,
+                                        self.current_z)
+
+            elif data.buttons[14] == 1:  # D-pad down
+                self.send_absolute_move(self.current_x - self.step_x,
+                                        self.current_yaw,
+                                        self.current_z)
+        if data.buttons[1] == 1:   # X button
+            if data.button[13] == 1:     # D-pad up
+                self.send_absolute_move(self.current_x,
+                                        self.current_yaw,
+                                        self.current_z + self.step_z)
+
+            elif data.buttons[14] == 1:  # D-pad down
+                self.send_absolute_move(self.current_x,
+                                        self.current_yaw,
+                                        self.current_z - self.step_z)
+        if data.buttons[3] == 1:   # X button
+            if data.button[12] == 1:     # D-pad right
+                self.send_absolute_move(self.current_x,
+                                        self.current_yaw + self.step_yaw,
+                                        self.current_z)
+
+            elif data.buttons[11] == 1:  # D-pad down
+                self.send_absolute_move(self.current_x,
+                                        self.current_yaw - self.step_yaw,
+                                        self.current_z)
+        # if data.buttons[1] == 1 and self.prev_buttons[1] == 0:
+        #     self.send_move_goal(0.05, 0.05, 0.1)
+        #     self.prev_buttons = data.buttons
+        # if data.buttons[2] == 1 and self.prev_buttons[2] == 0:
+        #     self.send_move_goal(0.05, 0.05, 0.05)
+        #     self.prev_buttons = data.buttons
+        # if data.buttons[3] == 1 and self.prev_buttons[3] == 0:
+        #     self.send_move_goal(0.05, 0.05, 0.01)
+        #     self.prev_buttons = data.buttons
         if self.saved_img and self.release_img:
             message_type = 15
             message = struct.pack('!BQ', message_type,0)
